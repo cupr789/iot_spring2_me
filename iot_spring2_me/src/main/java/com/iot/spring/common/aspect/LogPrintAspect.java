56b4e776da4a7fb6df9104aea92137d7b6fa6d04
cupr7 <cupr7@DESKTOP-SLR5M5U>
2018-02-18 14:15:34 +0900

    2018-02-18 네이버 콘솔창에 찍힘,간단 query cLay에 출력 완성,pom.xml 두개남음

---
+++ iot_spring2_me/src/main/java/com/iot/spring/common/aspect/LogPrintAspect.java
@@ -0,0 +1,59 @@
+package com.iot.spring.common.aspect;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.iot.spring.dao.NaverTransDAO;
+import com.iot.spring.dao.impl.NaverTransDAOImpl;
+
+@Service
+@Aspect
+public class LogPrintAspect {
+	
+	@Autowired
+	NaverTransDAO ntDao;
+	
+	
+	private static final Logger log = LoggerFactory.getLogger(LogPrintAspect.class);
+	@Before("execution(* com.iot.spring.controller.*Controller.*(..))")
+	public void beforeLog(JoinPoint jp) {
+		log.info("@Before =>{}",jp);
+	}
+	
+	@Around("execution(* com.iot.spring.controller.*Controller.*(..))")
+	public Object aroundLog(ProceedingJoinPoint pjp) throws IOException {
+		log.info("@Around begin");
+		Object obj=null;
+		long startTime = System.currentTimeMillis();
+		try {
+			obj=pjp.proceed();
+			log.info("정상진행? =>{}",obj);
+		} catch (Throwable e) {
+			log.error("@Around error=>{}",e);
+			ModelAndView mav = new ModelAndView();
+			mav.addObject("errorMsg",ntDao.getText(e.getMessage()));
+			log.error("mav???? =>{}",mav);
+			return mav;
+		}
+		log.info("@Around end, RunTime : {} ms",System.currentTimeMillis()-startTime);
+		return obj;
+	}
+	
+	@After("execution(* com.iot.spring.controller.*Controller.*(..))")
+	public void afterLog(JoinPoint jp) {
+		log.info("@after =>{}",jp);
+	}
+}
---
+++ iot_spring2_me/src/main/java/com/iot/spring/vo/SqlVO.java
@@ -0,0 +1,20 @@
+package com.iot.spring.vo;
+
+public class SqlVO {
+	private String sql;
+
+	public String getSql() {
+		return sql;
+	}
+
+	public void setSql(String sql) {
+		this.sql = sql;
+	}
+
+	@Override
+	public String toString() {
+		return "SqlVO [sql=" + sql + "]";
+	}
+	
+	
+}
